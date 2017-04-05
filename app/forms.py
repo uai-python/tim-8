7eb21380431fd0bf81fec399e2adf6905e198f14
Faryzal2020 <fary290796@gmail.com>
2017-04-05 21:58:21 +0700

    delet
    unneeded

--- app/forms.py
+++
@@ -1,6 +0,0 @@
-from flask_wtf import Form
-from wtforms import StringField, BooleanField
-from wtforms.validators import DataRequired
-
-class QuizForm(Form):
-	answer = StringField('answer', validators=[DataRequired()])
